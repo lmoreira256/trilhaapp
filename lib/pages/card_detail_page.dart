eecda01722d595d609fa93eafcb30eea68128721
Lucas Moreira <lucamoreira256@gmail.com>
2024-11-07 16:27:17 -0300

    Implementado Hero

---
+++ lib/pages/card_detail_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:trilhaapp/model/card_detail.dart';
+
+class CardDetailPage extends StatelessWidget {
+  final CardDetail cardDetail;
+
+  const CardDetailPage({
+    super.key,
+    required this.cardDetail,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: cardDetail.id,
+      child: SafeArea(
+        child: Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(Icons.close),
+                ),
+                Row(
+                  children: [
+                    Image.network(
+                      cardDetail.url,
+                      height: 100,
+                    ),
+                  ],
+                ),
+                Text(
+                  cardDetail.title,
+                  style: const TextStyle(
+                      fontSize: 26, fontWeight: FontWeight.w700),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Expanded(
+                  child: Text(
+                    cardDetail.text,
+                    textAlign: TextAlign.justify,
+                    style: const TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:trilhaapp/model/card_detail.dart';
+import 'package:trilhaapp/pages/card_detail_page.dart';
 
 class CardPage extends StatefulWidget {
   const CardPage({super.key});
@@ -8,6 +10,13 @@ class CardPage extends StatefulWidget {
 }
 
 class _CardPageState extends State<CardPage> {
+  var cardDetail = CardDetail(
+    1,
+    'Meu Card',
+    'https://hermes.digitalinnovation.one/assets/diome/logo.png',
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  );
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -18,50 +27,65 @@ class _CardPageState extends State<CardPage> {
             horizontal: 16,
             vertical: 8,
           ),
-          child: Card(
-            elevation: 16,
-            shadowColor: Colors.red,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 8,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
+          child: InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CardDetailPage(
+                    cardDetail: cardDetail,
+                  ),
+                ),
+              );
+            },
+            child: Hero(
+              tag: cardDetail.id,
+              child: Card(
+                elevation: 16,
+                shadowColor: Colors.red,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 8,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Image.network(
-                        'https://hermes.digitalinnovation.one/assets/diome/logo.png',
-                        height: 20,
+                      Row(
+                        children: [
+                          Image.network(
+                            cardDetail.url,
+                            height: 20,
+                          ),
+                          Text(
+                            cardDetail.title,
+                            style: const TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w700),
+                          ),
+                        ],
                       ),
-                      const Text(
-                        'Meu Card',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w700),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        cardDetail.text,
+                        textAlign: TextAlign.justify,
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Ler mais',
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
                       ),
                     ],
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text(
-                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      'Ler mais',
-                      style: TextStyle(
-                        decoration: TextDecoration.underline,
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
           ),
